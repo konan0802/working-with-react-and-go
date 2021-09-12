e6fec2092bfdbcd0b6dee11a2ce8abf64dacdf2d
konan080211 <konanforbis@gmail.com>
2021-09-12 18:02:55 +0900

    first commit

---
+++ test-app/src/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import AppFooter from './AppFooter';
+import './index.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        <div>
+          <h1>Hello, world!</h1>
+        </div>
+        <AppFooter />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
