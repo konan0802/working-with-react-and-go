e4db9df41a9f1b5058d531ca35597aa1e1b10b32
konan080211 <konanforbis@gmail.com>
2021-09-22 08:06:13 +0900

    add: More events

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
+    anotherFunction = () => {
+        console.log("anotherFunction");
+    }
+
+    leftParagraph = () => {
+        console.log("left the paragraph");
+    }
+
     fetchList = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => response.json())
         .then(json => {
             console.log(json);
+            this.anotherFunction();
             let posts = document.getElementById("post-list");
 
             json.forEach(function(obj){
@@ -22,6 +31,8 @@ export default class AppContent extends Component {
             <p>
                 This is the content
                 <br />
+                <hr />
+                <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagraph}>This is some text</p>
                 <button onClick={this.fetchList} className="btn btn-primary">Fetch Data</button>
                 <hr />
                 <ul id="post-list"></ul>
