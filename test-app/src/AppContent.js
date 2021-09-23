e91c0243ece3b84a5f0ba46200c7c60b2c16cdfd
konan080211 <konanforbis@gmail.com>
2021-09-23 13:42:44 +0900

    fix: share data between components

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -2,26 +2,23 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
-    state = {posts: []};
-
     constructor(props) {
         super(props);
-        this.listRef = React.createRef();
+        this.handlePostChange = this.handlePostChange.bind(this);
     }
 
-    anotherFunction = () => {
-        console.log("anotherFunction");
+    handlePostChange(posts) {
+        this.props.handlePostChange(posts);
     }
 
-    leftParagraph = () => {
-        console.log("left the paragraph");
-    }
+    state = {posts: []};
 
     fetchList = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => response.json())
         .then(json => {
             this.setState({posts: json});
+            this.handlePostChange(json);
         })
     }
 
@@ -35,7 +32,6 @@ export default class AppContent extends Component {
                 This is the content
                 <br />
                 <hr />
-                <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagraph}>This is some text</p>
                 <button onClick={this.fetchList} className="btn btn-primary">Fetch Data</button>
                 <hr />
                 <p>Posts is {this.state.posts.length} items long</p>
--- test-app/src/AppHeader.js
+++ test-app/src/AppHeader.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 //import './AppHeader.css';
 
 export default class AppHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handlePostChange = this.handlePostChange.bind(this);
+    }
+
+    handlePostChange(posts) {
+        this.props.handlePostChange(posts);
+    }
+
     render() {
         return (
-            <h1>{this.props.favorite_color}</h1>
+            <Fragment>
+                <h1>{this.props.favorite_color}</h1>
+                <p>There are {this.props.posts.length} entire in posts</p>
+            </Fragment>
         )
     }
 }
