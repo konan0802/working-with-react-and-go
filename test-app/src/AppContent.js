224cf769e5294c786efe6369196e04fd77b8e06e
konan080211 <konanforbis@gmail.com>
2021-09-23 12:46:13 +0900

    add: state

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
+    state = {posts: []};
+
     constructor(props) {
         super(props);
         this.listRef = React.createRef();
@@ -19,30 +21,34 @@ export default class AppContent extends Component {
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => response.json())
         .then(json => {
-            console.log(json);
-            this.anotherFunction();
-            // let posts = document.getElementById("post-list");
-            const posts = this.listRef.current;
-
-            json.forEach(function(obj){
-                let li = document.createElement("li");
-                li.appendChild(document.createTextNode(obj.title));
-                posts.appendChild(li);
-            })
+            this.setState({posts: json});
         })
+    }
+
+    clickedItme = (x) => {
+        console.log("clicked", x);
     }
 
     render(){
         return (
-            <p>
+            <div>
                 This is the content
                 <br />
                 <hr />
                 <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagraph}>This is some text</p>
                 <button onClick={this.fetchList} className="btn btn-primary">Fetch Data</button>
                 <hr />
-                <ul id="post-list" ref={this.listRef}></ul>
-            </p>
+                <p>Posts is {this.state.posts.length} items long</p>
+                <ul>
+                    {this.state.posts.map((c) => (
+                        <li key={c.id}>
+                            <a href="#!" onClick={() => this.clickedItme(c.id)}>
+                                {c.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
