c37daee2c030eda1074d362aa573a3ce5d729956
konan080211 <konanforbis@gmail.com>
2021-09-26 13:55:40 +0900

    add: フロントからバックエンドにアクセス

--- go-movies/src/components/Movies.js
+++ go-movies/src/components/Movies.js
@@ -3,30 +3,41 @@ import { Link } from 'react-router-dom';
 
 export default class Movies extends Component {
 
-    state = {movies: []};
+    state = {
+        movies: [],
+        isLoaded: false,
+    };
 
     componentDidMount() {
-        this.setState({
-            movies: [
-                {id: 1, title: "アベンジャーズ", runtime: 142},
-                {id: 2, title: "フォルトゥナの瞳", runtime: 175},
-                {id: 3, title: "天気の子", runtime: 160},
-            ]
-        })
+        fetch("http://localhost:4000/v1/movies")
+        .then((response) => response.json())
+        .then((json => {
+            this.setState({
+                movies: json.movies,
+                isLoaded: true,
+            })
+        }))
     }
 
     render() {
-        return (
-            <Fragment>
-                <h2>Choose a mivie</h2>
-                <ul>
-                    {this.state.movies.map((m) => (
-                        <li key={m.id}>
-                            <Link to={`movies/${m.id}`}>{m.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </Fragment>
-        );
+        const{ movies, isLoaded } = this.state;
+
+        if (!isLoaded) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Choose a mivie</h2>
+                    
+                    <ul>
+                        {this.state.movies.map((m) => (
+                            <li key={m.id}>
+                                <Link to={`movies/${m.id}`}>{m.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </Fragment>
+            );
+        }
     }
 }
